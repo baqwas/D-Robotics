6cd03d1261aadb20ff5c2d920e19d4269c0fb8be
Matha <baqwas@gmail.com>
2025-01-11 19:57:07 -0600

    USB camera read write test

---
+++ src/camera/image_read_write.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_efficientnasnet_m.py
+Use EfficientNet-m to classify an image
+
+This file is part of d-robotics repository at https://github.com/baqwas/d-robotics
+It is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not,
+see <https://www.gnu.org/licenses/>.
+
+(C) 2024 ParkCircus Productions; All Rights Reserved
+
+Efficient-NASNet-M focuses on efficiency and accuracy through:
+- Efficient-NASNet-M leverages EfficientNet scaling moethod. (depth, width & resolution)
+- NASNet architecture
+- Improved resource usage
+- Better performance without compromising accuracy
+
+Adapted from DF Robot RDK X3 documentation
+@sa https://arxiv.org/pdf/1905.11946
+@sa https://colab.research.google.com/github/d2l-ai/d2l-pytorch-colab/blob/master/chapter_convolutional-modern/googlenet.ipynb
+"""
+
+import argparse
+import numpy as np
+import cv2
+
+def image_read_write(file_name):
+    # 打开 usb camera: /dev/video8
+    cap = cv2.VideoCapture(8)
+    if cap.isOpened():
+        print("USB camera opened")
+    else:
+        exit(-1)
+
+    """
+    Common FourCC codes:
+        'XVID': A widely supported codec for AVI files
+        'MJPG': Motion JPEG codec, often used for higher quality output
+        'DIVX': Another popular codec for AVI files
+        'MP4V': For MP4 files
+        'H264': A modern and efficient codec, often used for high-quality video
+
+    """
+    # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    # codec = cv2.VideoWriter('output.mp4', fourcc, 30.0, (1920, 1080))
+    codec = cv2.VideoWriter_fourcc( 'M', 'J', 'P', 'G' )
+    cap.set(cv2.CAP_PROP_FOURCC, codec)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+    _ ,frame = cap.read()
+
+    # print(frame.shape)
+
+    if frame is None:
+        print("Failed to get image from usb camera")
+
+    cv2.imwrite(file_name, frame)
+    return
+
+if __name__ == '__main__':
+    print(f"image_read_write: v0.1")
+    parser = argparse.ArgumentParser(description="Test EfficientNet-m model classification")
+    parser.add_argument("image_file", type=str, help="Path to the image file to be classified",
+                        default="/home/sunrise/PycharmProjects/d-robotics/src/camera/image_read_write.jpg")
+    args = parser.parse_args()
+    image_file = args.image_file
+    print(f"Current frame from camera to be written as {image_file} in current folder")
+
+    image_read_write(image_file)
